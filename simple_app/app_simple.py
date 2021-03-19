edb598598fe68326fce9dbcce566395e280e0b39
Will-Cooper <w.cooper@herts.ac.uk>
2021-03-19 14:08:43 +0000

    Made page buttons work, did some minor wording changes to make relevant for SIMPLE

--- simple_app/app_simple.py
+++ simple_app/app_simple.py
@@ -11,23 +11,32 @@ app_simple.vars['specid'] = ''
 app_simple.vars['source_id'] = ''
 
 db_file = 'sqlite:///../SIMPLE.db'
-db = Database(db_file)
 pd.set_option('max_colwidth', None)  # deprecation warning
 
 
 # website pathing
 @app_simple.route('/')
-@app_simple.route('/index')
 @app_simple.route('/index', methods=['GET', 'POST'])
-def simple_query():
+def index_page():
+    db = Database(db_file)
     defquery = 'SELECT * FROM sources'
     if app_simple.vars['query'] == '':
         app_simple.vars['query'] = defquery
 
     source_count = db.query(db.Sources).count()
     test_query = db.search_object('twa 27', fmt='pandas').source.values
-
+    del db  # sqlalchemy starts complaining about threads otherwise
     return render_template('index_simple.html', source_count=source_count, test_query=test_query)
+
+
+@app_simple.route('/feedback')
+def feedback_page():
+    return render_template('feedback.html')
+
+
+@app_simple.route('/schema')
+def schema_page():
+    return render_template('schema.html')
 
 
 if __name__ == '__main__':
