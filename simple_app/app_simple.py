fa40f53c299bc68053a08e813a28be5eb39b5974
Will-Cooper <w.cooper@herts.ac.uk>
2021-04-06 13:14:10 +0100

    Documenting line by line and clearing some unused lines (still runs) and add argparse

--- simple_app/app_simple.py
+++ simple_app/app_simple.py
@@ -1,46 +1,66 @@
+"""
+This is the main script to be run from the directory root, it will start the Flask application running which one can
+then connect to.
+"""
+# external packages
 from astrodbkit2.astrodb import Database
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_wtf import FlaskForm
 from markdown2 import markdown
-import os
+import numpy as np
 import pandas as pd
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, StopValidation
+# internal packages
+import argparse
+import os
 
 # initialise
 app_simple = Flask(__name__)
-app_simple.vars = dict()
-app_simple.vars['query'] = ''
-app_simple.vars['search'] = ''
-app_simple.vars['specid'] = ''
-app_simple.vars['source_id'] = ''
-app_simple.config['SECRET_KEY'] = os.urandom(32)
+app_simple.config['SECRET_KEY'] = os.urandom(32)  # need to generate csrf token as basic security for Flask
 
-db_file = 'sqlite:///../SIMPLE.db'
-pd.set_option('max_colwidth', None)  # deprecation warning
+
+def sysargs():
+    """
+    These are the system arguments given after calling this python script
+
+    Returns
+    -------
+    _args
+        The different argument parameters, can be grabbed via their long names (e.g. _args.host)
+    """
+    _args = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    _args.add_argument('-i', '--host', default='127.0.0.1', help='Local IP Address to host server')
+    _args.add_argument('-p', '--port', default=8000, help='Local port number to host server through', type=int)
+    _args.add_argument('-d', '--debug', help='Run Flask in debug mode?', default=False, action='store_true')
+    _args.add_argument('-f', '--file', help='Database file path relative to current directory', default='SIMPLE.db')
+    _args = _args.parse_args()
+    return _args
 
 
 class SimpleDB(Database):  # this keeps pycharm happy about unresolved references
-    Sources = None
+    Sources = None  # initialise class attribute
 
 
 class CheckResultsLength(object):
     def __call__(self, form, field):
-        db = SimpleDB(db_file, connection_arguments={'check_same_thread': False})
-        results: pd.DataFrame = db.search_object(field.data, fmt='pandas')
-        if not len(results):
-            field.errors[:] = []
-            raise StopValidation(field.gettext('No results'))
+        db = SimpleDB(db_file, connection_arguments={'check_same_thread': False})  # open database
+        results: pd.DataFrame = db.search_object(field.data, fmt='pandas')  # search by what is currently in searchbar
+        if not len(results):  # if that search is empty
+            field.errors[:] = []  # clear existing errors
+            raise StopValidation(field.gettext('No results'))  # stop validating and return error
 
 
 class SearchForm(FlaskForm):
-    search = StringField('', [DataRequired(), CheckResultsLength()], id='autocomplete')
-    submit = SubmitField('Query')
+    search = StringField('', [DataRequired(), CheckResultsLength()], id='autocomplete')  # searchbar
+    submit = SubmitField('Query')  # clicker button to send request
 
 
-_db = SimpleDB(db_file, connection_arguments={'check_same_thread': False})
-allresults = _db.search_object('', fmt='pandas').source.values.tolist()
-del _db
+def all_sources():
+    db = SimpleDB(db_file, connection_arguments={'check_same_thread': False})  # open database
+    # FIXME: db.search_object(..fmt='pandas') not returning column names
+    allresults = np.array(db.search_object(''))[:, 0].tolist()  # list all the sources by main name
+    return allresults
 
 
 # website pathing
@@ -49,43 +69,43 @@ del _db
 def index_page():
     # open database object, use connection arguments to have different threads to calm sqlite
     db = SimpleDB(db_file, connection_arguments={'check_same_thread': False})
-    source_count = db.query(db.Sources).count()
+    source_count = db.query(db.Sources).count()  # count the number of sources
     return render_template('index_simple.html', source_count=source_count)
 
 
 @app_simple.route('/search', methods=['GET', 'POST'])
 def search():
-    form = SearchForm()
-    query = form.search.data
-    db = SimpleDB(db_file, connection_arguments={'check_same_thread': False})
-    results: pd.DataFrame = db.search_object(query, fmt='pandas')
-    if request.method == 'POST' and form.validate_on_submit():
-        if len(results) > 1:
-            return redirect((url_for('search_results', query=query)))
-        return redirect((url_for('solo_result', query=query)))
-    return render_template('search.html', form=form)
+    form = SearchForm()  # searchbar
+    query = form.search.data  # the content in searchbar
+    db = SimpleDB(db_file, connection_arguments={'check_same_thread': False})  # open database
+    results: pd.DataFrame = db.search_object(query, fmt='pandas')  # get the results for that object
+    if request.method == 'POST' and form.validate_on_submit():  # if everything okay with the search
+        if len(results) > 1:  # if more than one result
+            return redirect((url_for('search_results', query=query)))  # return table of all results
+        return redirect((url_for('solo_result', query=query)))  # otherwise return page for that one object
+    return render_template('search.html', form=form)  # if everything not okay, return existing page as is
 
 
 @app_simple.route('/search_results/<query>')
 def search_results(query: str):
-    db = SimpleDB(db_file, connection_arguments={'check_same_thread': False})
-    results: pd.DataFrame = db.search_object(query, fmt='pandas')
-    query = query.upper()
-    results: str = markdown(results.to_html())
+    db = SimpleDB(db_file, connection_arguments={'check_same_thread': False})  # open database
+    results: pd.DataFrame = db.search_object(query, fmt='pandas')  # get all results for that object
+    query = query.upper()  # convert contents of search bar to all upper case
+    results: str = markdown(results.to_html())  # convert the results into markdown to display nice on page
     return render_template('search_results.html', query=query, results=results)
 
 
 @app_simple.route('/solo_result/<query>')
 def solo_result(query: str):
-    db = SimpleDB(db_file, connection_arguments={'check_same_thread': False})
-    resultdict: dict = db.inventory(query)
-    query = query.upper()
+    db = SimpleDB(db_file, connection_arguments={'check_same_thread': False})  # open database
+    resultdict: dict = db.inventory(query)  # get everything about that object
+    query = query.upper()  # convert query to all upper case
     return render_template('solo_result.html', query=query, resultdict=resultdict)
 
 
 @app_simple.route('/autocomplete', methods=['GET'])
 def autocomplete():
-    return jsonify(alljsonlist=allresults)
+    return jsonify(alljsonlist=all_results)  # wraps all of the object names as a list, into a .json for server use
 
 
 @app_simple.route('/feedback')
@@ -99,4 +119,7 @@ def schema_page():
 
 
 if __name__ == '__main__':
-    app_simple.run(host='127.0.0.1', port=8000, debug=True)
+    args = sysargs()  # get all system arguments
+    db_file = f'sqlite:///{args.file}'  # the database file
+    all_results = all_sources()  # find all the objects once
+    app_simple.run(host=args.host, port=args.port, debug=args.debug)  # generate the application on server side
