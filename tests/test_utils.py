6c6864331d4309641f66e607d61056449e8b8b7b
Will-Cooper <w.cooper@herts.ac.uk>
2021-09-27 17:11:38 +0100

    Basic tests for utils functions, closes #27

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,14 @@
 """
 Testing the utils functions
+
+Non directly tested functions:
+find_colours -- called by all_photometry
+parse_photometry -- called by all_photometry
+absmags -- called by results_concat
+coordinate_project -- called by results_concat
 """
 # external packages
+import pandas as pd
 import pytest
 # internal packages
 import os
@@ -14,7 +21,7 @@ db_name = 'temp.db'
 db_cs = 'sqlite:///temp.db'
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def db():
     if os.path.exists(db_name):
         os.remove(db_name)
@@ -27,9 +34,53 @@ def db():
     return db
 
 
+@pytest.fixture(scope='module')
 def test_all_sources(db):
     assert db
     assert all_sources(db_cs)
+    allresults, fullresults = all_sources(db_cs)
+    assert len(allresults) and len(fullresults)
+    assert type(allresults) == list
+    assert type(fullresults) == pd.DataFrame
+    return allresults, fullresults
+
+
+@pytest.fixture(scope='module')
+def test_all_photometry(db):
+    assert db
+    allphoto, allbands = all_photometry(db_cs)
+    assert len(allphoto) and len(allbands)
+    assert type(allphoto) == pd.DataFrame
+    assert type(allbands) == np.ndarray
+    return allphoto, allbands
+
+
+@pytest.fixture(scope='module')
+def test_all_parallaxes(db):
+    assert db
+    allplx = all_parallaxes(db_cs)
+    assert len(allplx)
+    assert type(allplx) == pd.DataFrame
+    return allplx
+
+
+def test_find_colours(db, test_all_photometry):
+    assert db
+    allphoto, allbands = test_all_photometry
+    photodf = find_colours(allphoto, allbands)
+    assert len(photodf)
+    assert type(photodf) == pd.DataFrame
+    return
+
+
+def test_results_concat(db, test_all_photometry, test_all_sources, test_all_parallaxes):
+    assert db
+    allphoto, allbands = test_all_photometry
+    allresults, fullresults = test_all_sources
+    allplx = test_all_parallaxes
+    allresultsconcat = results_concat(fullresults, allphoto, allplx, allbands)
+    assert all([col in allresultsconcat.columns for col in ('dist', 'raproj', 'decproj')])
+    assert all([f'M_{band}' in allresultsconcat.columns for band in allbands])
     return
 
 
