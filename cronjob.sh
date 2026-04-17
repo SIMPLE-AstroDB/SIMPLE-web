42fd5217359c3090a6fb815ef12b6816fe2c04e7
Will-Cooper <w.cooper@herts.ac.uk>
2026-04-17 10:43:32 +0100

    add cronjob to git, closes #212

---
+++ cronjob.sh
@@ -0,0 +1,4 @@
+cd ~/ROOT
+wget -O SIMPLE.sqlite https://raw.githubusercontent.com/SIMPLE-AstroDB/SIMPLE-binary/main/SIMPLE.sqlite
+wget -O database.toml https://raw.githubusercontent.com/SIMPLE-AstroDB/SIMPLE-db/refs/heads/main/database.toml
+git pull
